426001ba99455cc18bc45e97620b939ff947b8fa
GhostMan15 <farukuzicanin1@gmail.com>
2026-05-06 19:57:13 +0200

    Fixed logic for food making player too big

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -5,7 +6,14 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public GameObject foodPrefab;
     public Vector2 xRange, yRange;
+    public float minDistance = 1.5f, size = 1f, maxSize = 3f, growthPerFood  = 0.1f;
+    public int currentFood = 0, maxFood = 100;
 
+    public void Grow()
+    {
+        size += growthPerFood;
+        size = Mathf.Clamp(size, 1f, maxSize);
+    }
 
     private void Awake()
     {
@@ -21,13 +29,32 @@ public class GameManager : MonoBehaviour
     }
     public void SpawnFood()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(xRange.x, yRange.y), Random.Range(xRange.x, yRange.y), 1);
+
+        Vector3 spawnPosition;
+        int attempts = 0;
+        bool found = false;
+        if(currentFood >= maxFood) return;
+    
+        do
+        {
+            spawnPosition = new Vector3(Random.Range(xRange.x, xRange.y), Random.Range(yRange.x, yRange.y), 1);
+            if (Physics2D.OverlapCircle(spawnPosition, minDistance))
+            {
+                found = true;   
+                break;
+            }
+            attempts++;
+        } while (attempts < 20);
+       if (!found) return;
         GameObject _food = Instantiate(foodPrefab, spawnPosition, Quaternion.identity);
         _food.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0f,1f,0.9f,1f,0.9f,1f);
+        currentFood++;
     }
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one * size, Time.deltaTime * 5f);
     }
+  
 }
+
