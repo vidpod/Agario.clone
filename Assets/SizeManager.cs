323fcc83e07940f56c52784da7b95adfe9df7765
D1stress <elmasdedic290@gmail.com>
2026-05-30 23:18:37 +0200

    Dodana logika za collision med igralcem in botom

--- Assets/SizeManager.cs
+++ Assets/SizeManager.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class SizeManager : MonoBehaviour
 {
     public float scaleSpeed = 5f;
-    public float currentScale = 1f,growthPerFood = 0.5f;
+    public float currentScale = 1f, growthPerFood = 0.5f;
+    public float combatGrowthFactor = 0.2f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -14,6 +15,26 @@ public class SizeManager : MonoBehaviour
             Destroy(other.gameObject);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SizeManager otherCharacter = collision.gameObject.GetComponent<SizeManager>();
+
+        if (otherCharacter != null)
+        {
+            
+            if (this.currentScale > otherCharacter.currentScale)
+            {
+             
+                currentScale += otherCharacter.currentScale * combatGrowthFactor;
+
+                Debug.Log($"[USPEH] {gameObject.name} je pojedel {collision.gameObject.name}. Nova ciljna velikost: {currentScale}");
+
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+
     void Update()
     {
         transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(currentScale, currentScale,1), Time.deltaTime * scaleSpeed);
